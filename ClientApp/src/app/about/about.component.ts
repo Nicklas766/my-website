9868e2d444260bce77a6b740efb5b815949b1635
Nicklas766 <Nicklas766@live.se>
2018-06-15 18:56:20 +0200

    setting up frontend

---
+++ ClientApp/src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
+})
+/** about component*/
+export class AboutComponent {
+    public testText: string = "About page";
+    /** about ctor */
+    constructor() {}
+
+  ngOnInit(): void {
+  }
+}
